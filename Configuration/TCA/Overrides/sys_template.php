cb5b56cf43539328fab558099a15c45ab17b92ed
Nanna Ellegaard <nel@adm.ku.dk>
2023-06-14 08:34:32 +0200

    Lang

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * This file is part of the package ucph_base_config.
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ * June 2023 Nanna Ellegaard, University of Copenhagen.
+ */
 
 defined('TYPO3') or die('Access denied.');
 
