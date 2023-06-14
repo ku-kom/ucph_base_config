8b70319bd7be9a1cc13ae6d0327efd2ac1d59afb
Nanna Ellegaard <nel@adm.ku.dk>
2023-06-14 15:06:36 +0200

    Backend stylesheet

--- ext_tables.php
+++ ext_tables.php
@@ -1,10 +1,9 @@
 <?php
 
 /*
- * This file is part of the package ku_prototype.
+ * This file is part of the package ucph_base_config.
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
- * Sep 2022 Nanna Ellegaard, University of Copenhagen.
  */
 
 defined('TYPO3') || die();
