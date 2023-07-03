eabe5856525cce48f0dd86c3d998e90e4710665f
Nanna Ellegaard <nel@adm.ku.dk>
2023-07-03 15:01:12 +0200

    Site width

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -4,28 +4,29 @@
  * This file is part of the package ucph_base_config.
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
- * June 2023 Nanna Ellegaard, University of Copenhagen.
  */
 
 defined('TYPO3') or die();
 
 (function () {
-    $tempColumns = [
-       'full_site_width' => [
-           'label' => 'LLL:EXT:ucph_base_config/Resources/Private/Language/locallang_be.xlf:tt_content.full_site_width.title',
-           'config' => [
-               'type' => 'check',
-           ]
-       ],
-    ];
-
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('tt_content', $tempColumns, 1);
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+        'tt_content',
+        [
+            'full_site_width' => [
+                'exclude' => true,
+                'label' => 'LLL:EXT:ucph_base_config/Resources/Private/Language/locallang_be.xlf:tt_content.full_site_width.title',
+                'config' => [
+                    'type' => 'check',
+                ]
+            ],
+        ]
+    );
 
     // Add in tab "Appearence"
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette(
         'tt_content',
+        'frames',
         'full_site_width',
-        '',
         'after:space_after_class'
     );
 })();
