a422c92e21fde3799a382ed2513e8b0566e2cd97
Nanna Ellegaard <nel@adm.ku.dk>
2023-07-04 14:14:06 +0200

    Setup

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,15 +5,6 @@
 -- Table structure for table 'pages'
 --
 CREATE TABLE pages (
-   # Table for UCPH faculty definition on site root:
-   ucph_color_theme varchar(255) DEFAULT '' NOT NULL,
    # Last updated page time stamp
    ucph_lastpageupdates_timestamp int(11) default '0' NOT NULL
-);
-
---
--- Table structure for table 'tt_content'
---
-CREATE TABLE tt_content (
-   full_site_width VARCHAR(255) DEFAULT '' NOT NULL
 );
