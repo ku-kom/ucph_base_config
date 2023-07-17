310083d9ca42d46561dd5774f90663e6951d4e56
Nanna Ellegaard <nel@adm.ku.dk>
2023-07-17 10:05:54 +0200

    Default cropping values

--- Configuration/TCA/Overrides/sys_file_reference.php
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -23,8 +23,8 @@ if (!isset($GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config']['c
 $GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config']['cropVariants'] = array_replace_recursive(
     $GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config']['cropVariants'],
     [
-        'Desktop' => [
-            'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:imgsize-desktop',
+        'default' => [
+            'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:option.default',
             'allowedAspectRatios' => [
                 '16:9' => [
                     'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.16_9',
@@ -65,8 +65,8 @@ $GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config']['cropVariants
                 ]
             ],
         ],
-        'Tablet' => [
-            'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:imgsize-tablet',
+        'xlarge' => [
+            'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:option.xlarge',
             'allowedAspectRatios' => [
                 '16:9' => [
                     'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.16_9',
@@ -107,8 +107,134 @@ $GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config']['cropVariants
                 ]
             ],
         ],
-        'Mobile' => [
-            'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:imgsize-mobile',
+        'large' => [
+            'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:option.large',
+            'allowedAspectRatios' => [
+                '16:9' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.16_9',
+                    'value' => 16 / 9
+                ],
+                '1:1' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.1_1',
+                    'value' => 1.0
+                ],
+                '4:3' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.4_3',
+                    'value' => 4 / 3
+                 ],
+                 'NaN' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.free',
+                    'value' => 0.0
+                 ],
+            ],
+            'selectedRatio' => '16:9',
+            'cropArea' => [
+                'x' => 0.0,
+                'y' => 0.0,
+                'width' => 1.0,
+                'height' => 1.0,
+            ],
+            'focusArea' => [
+                'x' => 1 / 3,
+                'y' => 1 / 3,
+                'width' => 1 / 3,
+                'height' => 1 / 3,
+            ],
+            'coverAreas' => [
+                [
+                    'x' => 0.05,
+                    'y' => 0.85,
+                    'width' => 0.9,
+                    'height' => 0.1,
+                ]
+            ],
+        ],
+        'medium' => [
+            'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:option.medium',
+            'allowedAspectRatios' => [
+                '16:9' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.16_9',
+                    'value' => 16 / 9
+                ],
+                '1:1' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.1_1',
+                    'value' => 1.0
+                ],
+                '4:3' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.4_3',
+                    'value' => 4 / 3
+                 ],
+                 'NaN' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.free',
+                    'value' => 0.0
+                 ],
+            ],
+            'selectedRatio' => '4:3',
+            'cropArea' => [
+                'x' => 0.0,
+                'y' => 0.0,
+                'width' => 1.0,
+                'height' => 1.0,
+            ],
+            'focusArea' => [
+                'x' => 1 / 3,
+                'y' => 1 / 3,
+                'width' => 1 / 3,
+                'height' => 1 / 3,
+            ],
+            'coverAreas' => [
+                [
+                    'x' => 0.05,
+                    'y' => 0.85,
+                    'width' => 0.9,
+                    'height' => 0.1,
+                ]
+            ],
+        ],
+        'small' => [
+            'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:option.small',
+            'allowedAspectRatios' => [
+                '16:9' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.16_9',
+                    'value' => 16 / 9
+                ],
+                '1:1' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.1_1',
+                    'value' => 1.0
+                ],
+                '4:3' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.4_3',
+                    'value' => 4 / 3
+                 ],
+                 'NaN' => [
+                    'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.free',
+                    'value' => 0.0
+                 ],
+            ],
+            'selectedRatio' => '1:1',
+            'cropArea' => [
+                'x' => 0.0,
+                'y' => 0.0,
+                'width' => 1.0,
+                'height' => 1.0,
+            ],
+            'focusArea' => [
+                'x' => 1 / 3,
+                'y' => 1 / 3,
+                'width' => 1 / 3,
+                'height' => 1 / 3,
+            ],
+            'coverAreas' => [
+                [
+                    'x' => 0.05,
+                    'y' => 0.85,
+                    'width' => 0.9,
+                    'height' => 0.1,
+                ]
+            ],
+        ],
+        'extrasmall' => [
+            'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:option.extrasmall',
             'allowedAspectRatios' => [
                 '16:9' => [
                     'title' => 'LLL:EXT:'. $ext .'/Resources/Private/Language/locallang_be.xlf:ratio.16_9',
